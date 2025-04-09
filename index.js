f0f52ed80cbf2678041a1d40281c0e22f11c53f8
DaffaPermadi <daffagt123@gmail.com>
2025-04-09 13:48:09 +0700

    Complete answer in index.js

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
+//Soal 1
 let Person = {
     name: "John",
     age: 25,
     job: "Developer"
 }
 
+//Soal 2
 console.log(Person.name); 
 console.log(Person.job); 
 
+//Soal 3
 Person.hobby = "Reading";
 console.log(Person.hobby); 
 
+//Soal 4
+Person.age = 30
+console.log(Person.age);
+
+//Soal 5
 delete Person.job;
 console.log(Person); 
 
+//Soal 6
 let Student = {
     name: "Alice",
     grade: 90,
@@ -23,6 +32,9 @@ let Student = {
 }
 console.log(Student.address.city)
 
+
+
+//Soal 7
 Person.greet = function (){
     return `Hi, my name is ${this.name}`
 }
