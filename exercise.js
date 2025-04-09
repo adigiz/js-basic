0dd74a170f7f70613543c1f9da2c3815af563902
muafi <nmuafi1@gmail.com>
2025-04-09 16:58:25 +0700

    fix: update feedback from code review

--- exercise.js
+++ exercise.js
@@ -1,20 +1,23 @@
+const pricing = {
+    car: {
+        firstHour: 5000,
+        nexthour: 3000,
+        extraCharge: 50000,
+    },
+    bike: {
+        firstHour: 2000,
+        nexthour: 1000,
+        extraCharge: 20000,
+    },
+};
+
 const lotBilling = (vehicleType, duration) => {
     if (vehicleType !== "car" && vehicleType !== "bike") {
         return "Invalid vehicle type";
     }
-    
-    const pricing = {
-        car: {
-            firstHour: 5000,
-            nexthour: 3000,
-            extraCharge: 50000,
-        },
-        bike: {
-            firstHour: 2000,
-            nexthour: 1000,
-            extraCharge: 20000,
-        },
-    };
+    if (duration <= 0) {
+        duration = 1;
+    }
     
     let totalCharge = duration > 24 ? pricing[vehicleType].extraCharge : 0;
     let totalFee = pricing[vehicleType].firstHour + (duration - 1) * pricing[vehicleType].nexthour + totalCharge;
@@ -25,4 +28,4 @@ const lotBilling = (vehicleType, duration) => {
 console.log(lotBilling("car", 3));
 console.log(lotBilling("bike", 1));
 console.log(lotBilling("car", 27));
-console.log(lotBilling("afjidhfha", 27));
+console.log(lotBilling("pesawat", 27));
